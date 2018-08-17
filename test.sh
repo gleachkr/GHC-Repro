da0f2c4270a28dbb342ecc9dc83eef90e22c2230
gleachkr <gleachkr@gmail.com>
2018-08-17 15:48:13 -0500

    Show contrast case in test

--- test.sh
+++ test.sh
@@ -4,3 +4,8 @@ stack build
 echo "These two results should be the same, since the only difference between the files is that one is an exported module"
 echo "test" | stack repl src/Tests/Good.hs -- 2> /dev/null | grep checkFlag 
 echo "test" | stack repl src/Tests/Bad.hs -- 2> /dev/null | grep checkFlag 
+echo "Here it is again with 8.4.3:"
+echo "Building project..."
+stack --stack-yaml stack-8.4.3.yaml build
+echo "test" | stack --stack-yaml stack-8.4.3.yaml repl src/Tests/Bad.hs -- 2> /dev/null | grep checkFlag 
+echo "test" | stack --stack-yaml stack-8.4.3.yaml repl src/Tests/Good.hs -- 2> /dev/null | grep checkFlag 
