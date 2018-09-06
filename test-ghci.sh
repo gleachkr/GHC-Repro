7b71166ecccba086ce41e31f643a0467aa45868c
gleachkr <gleachkr@gmail.com>
2018-09-06 10:38:21 -0500

    added .stack-work, modified paths in test-ghci.sh

--- test-ghci.sh
+++ test-ghci.sh
@@ -3,10 +3,10 @@ GHCI822="$HOME/.stack/programs/x86_64-linux/ghc-nopie-8.2.2/bin/ghci"
 GHCI843="$HOME/.stack/programs/x86_64-linux/ghc-nopie-8.4.3/bin/ghci"
 
 echo "Behavior in 8.2.2 with pkgdb"
-echo "test" | $GHCI822 -package-db ./pkgdb-8.2.2 src/Tests/Good.hs 2> /dev/null | grep checkFlag
+echo "test" | $GHCI822 -package-db ./.stack-work/install/x86_64-linux-nopie/lts-11.15/8.2.2/pkgdb src/Tests/Good.hs 2> /dev/null | grep checkFlag
 echo "Behavior in 8.2.2 without pkgdb"
 echo "test" | $GHCI822 src/Tests/Good.hs src/Tests/Link.hs 2> /dev/null | grep checkFlag
 echo "Behavior in 8.4.3 with pkgdb"
-echo "test" | $GHCI843 -package-db ./pkgdb-8.4.3 src/Tests/Good.hs 2> /dev/null | grep checkFlag
+echo "test" | $GHCI843 -package-db ./.stack-work/install/x86_64-linux-nopie/lts-12.6/8.4.3/pkgdb src/Tests/Good.hs 2> /dev/null | grep checkFlag
 echo "Behavior in 8.4.3 without pkgdb"
 echo "test" | $GHCI843 src/Tests/Good.hs src/Tests/Link.hs 2> /dev/null | grep checkFlag
